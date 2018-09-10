48998c9601586782554ece5f1b68c034159fc765
Andrea Németh <nemandreanemeth@gmail.com>
2018-09-10 14:55:51 +0200

    added imports

--- src/main/app/java/com/projectmanager/repository/TaskRepository.java
+++ src/main/app/java/com/projectmanager/repository/TaskRepository.java
@@ -1,5 +1,6 @@
 package com.projectmanager.repository;
 
+import com.projectmanager.model.TaskModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository extends JpaRepository<TaskModel, Integer>{
--- src/main/app/java/com/projectmanager/repository/TeamRepository.java
+++ src/main/app/java/com/projectmanager/repository/TeamRepository.java
@@ -1,5 +1,6 @@
 package com.projectmanager.repository;
 
+import com.projectmanager.model.TeamModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TeamRepository extends JpaRepository<TeamModel, Integer>{

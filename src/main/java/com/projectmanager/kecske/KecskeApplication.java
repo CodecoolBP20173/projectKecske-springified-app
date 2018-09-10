7b3d00a6ef9fb87e8aad3e4c01ac157531bfd0f6
Tovics <tovics1@gmail.com>
2018-09-10 13:01:02 +0200

    initial commit

---
+++ src/main/java/com/projectmanager/kecske/KecskeApplication.java
@@ -0,0 +1,12 @@
+package com.projectmanager.kecske;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KecskeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KecskeApplication.class, args);
+    }
+}

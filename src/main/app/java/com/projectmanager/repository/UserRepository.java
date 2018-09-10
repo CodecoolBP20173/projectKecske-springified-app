730302270e4c17b3e25c9200c3b8dcf9d427a85a
Horinka Peter <horinka.peter@gmail.com>
2018-09-10 14:22:23 +0200

    created a user repo

--- src/main/app/java/com/projectmanager/repository/UserRepository.java
+++ src/main/app/java/com/projectmanager/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.projectmanager.repository;
 
-public class UserRepository {
+import com.projectmanager.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserModel, Integer>{
 }

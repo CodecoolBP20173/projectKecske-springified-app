6c958a8c97dbf0be97366c01a576265bf0a487d5
Tovics <tovics1@gmail.com>
2018-09-10 13:22:34 +0200

    file hiearchy added, set env variables to db connection

--- src/main/app/java/com/projectmanager/KecskeApplication.java
+++ src/main/app/java/com/projectmanager/KecskeApplication.java
@@ -1,4 +1,4 @@
-package com.projectmanager.kecske;
+package com.projectmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

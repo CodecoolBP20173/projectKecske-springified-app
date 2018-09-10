044da61671cc138efae9cecdbded8243b1a09489
Horinka Peter <horinka.peter@gmail.com>
2018-09-10 14:25:31 +0200

    created a teamrepo

---
+++ src/main/app/java/com/projectmanager/repository/TeamRepository.java
@@ -0,0 +1,6 @@
+package com.projectmanager.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeamRepository extends JpaRepository<TeamModel, Integer>{
+}

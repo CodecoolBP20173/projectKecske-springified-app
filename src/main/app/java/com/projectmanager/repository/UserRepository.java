7bc3c1f7c769b52d52b37f39aea889f4cccef36d
Horinka Peter <horinka.peter@gmail.com>
2018-09-10 14:34:21 +0200

    added a task repo and a find by email method ti user repo

--- src/main/app/java/com/projectmanager/repository/UserRepository.java
+++ src/main/app/java/com/projectmanager/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.projectmanager.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserModel, Integer>{
+    UserModel findByEmail(String email);
 }

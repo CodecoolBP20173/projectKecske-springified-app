69b61e30e6b130136c458358b6952be86e632ac1
Tovics <tovics1@gmail.com>
2018-09-12 10:29:18 +0200

    index.html ports updated, user creation site can be reached from index page

--- src/main/app/java/com/projectmanager/controller/TeamController.java
+++ src/main/app/java/com/projectmanager/controller/TeamController.java
@@ -1,8 +1,23 @@
 package com.projectmanager.controller;
 
+import com.projectmanager.repository.TeamRepository;
+import com.projectmanager.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TeamController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TeamRepository teamRepository;
+
+    @GetMapping("/create_team")
+    public String teamForm() {
+        return "team_form";
+    }
 
 }

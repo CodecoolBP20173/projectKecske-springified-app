6af38b5aec1e98b4c1fe1fdfd367811408ce2821
Horinka Peter <horinka.peter@gmail.com>
2018-09-13 16:04:08 +0200

    index now lists the teams you are part of

--- src/main/app/java/com/projectmanager/controller/TeamController.java
+++ src/main/app/java/com/projectmanager/controller/TeamController.java
@@ -1,6 +1,8 @@
 package com.projectmanager.controller;
 
 import com.projectmanager.model.TeamModel;
+import com.projectmanager.model.UserModel;
+import com.projectmanager.repository.UserRepository;
 import com.projectmanager.service.TeamService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,8 +10,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class TeamController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    HttpSession session;
 
     @Autowired
     TeamService teamService;
@@ -21,7 +31,10 @@ public class TeamController {
 
     @PostMapping("/create_team")
     public String addNewTeam(@ModelAttribute TeamModel teamModel) {
+        UserModel user = (UserModel) session.getAttribute("user");
+        teamModel.setTeamLeader(user);
         teamService.addTeam(teamModel);
+
         return "redirect:/";
     }
 
